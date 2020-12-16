e974a0a0cf528deefe382e31e6593820212893a5
Priyal Sharma <58563788+Prl-1234@users.noreply.github.com>
2020-12-16 20:33:25 +0530

    new user dm
    DM new user on server join, random meme image and random meme quote added

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 // const Discord = require('discord.js')
 
 // const client=new Discord.Client();
-
 const Discord=require('discord.js');
 const client=new Discord.Client();
 
-const prefix='-'
+const prefix='.'
 
-const mine=require('minecraft-server-util')
+const mine=require('minecraft-server-util');
+const meme=require('random-stuff-api');
+const fetch=require('node-fetch');
+const queryString=require('querystring');
 
-// const fs=require('fs')
+const fs=require('fs')
 
-// client.commands=new Discord.Collection()
+client.commands=new Discord.Collection()
 
-// const commandFiles = fs.readdirSync('./commands/').filter(file =>file.endsWith('.js'))
-// for(const file of commandFiles){
-//     const command=require(`./commands/${file}`)
-//     client.commands.set(command.name,command)
-// }
+const commandFiles = fs.readdirSync('./commands/').filter(file =>file.endsWith('.js'))
+for(const file of commandFiles){
+    const command=require(`./commands/${file}`)
+    client.commands.set(command.name,command)
+}
 
 client.once('ready',()=>{
     console.log('Pomana is online')
+})
+client.on('guildMemberAdd',member=>{
+    member.send("Welcome to SWC Discord");
 })
 
 client.on('message',message=>{
@@ -29,13 +34,11 @@ client.on('message',message=>{
 
     const args =message.content.slice(prefix.length).split(/ +/);
     const command=args.shift().toLowerCase();
-    // if(command ==='ping'){
-    //     client.commands.get('ping').execute(message,args);
-    // }
-    // else if(command==='youtube'){
-    //     client.commands.get('youtube').execute(message,args);
-    // }
-    if(command==='mc'){
+    if(command ==='ping'){
+        client.commands.get('ping').execute(message,args);
+    }
+    
+    else if(command==='mc'){
         mine.status('play.hypixel.net')
             .then((response)=>{
                 var Embed=new Discord.MessageEmbed()
@@ -51,6 +54,48 @@ client.on('message',message=>{
                 throw error;
             });
     }
+    else if(command==='mgr'){
+        meme.random.insult()
+        .then(insult=>{
+            var Embed=new Discord.MessageEmbed()
+            .setTitle('Meme')
+            .setDescription(insult)
+            message.channel.send(Embed)
+            console.log(insult)
+        })
+    }
+    else if(command==='mgi'){
+        meme.image.meme()
+        .then(meme=>{
+            message.channel.send(meme)
+            console.log(meme)
+        })
+    }
+    else if(command==='cf'){
+        if (!args.length) {
+            return message.channel.send('You need to supply a search term!');
+            }
+            const query=queryString.stringify({handles:args.join(' ')});
+            const {result} =await fetch(`https://codeforces.com/api/user.info?${query}`).then(response => response.json());
+            // if (!result.length) {
+            //     return message.channel.send(`No results found for **${args.join(' ')}**.`);
+            // }
+        //   const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+          //  console.log(result[n-1].contestId);
+            var embed=new Discord.MessageEmbed()
+            .setColor('#FF6347')
+            .setTitle("User "+args)
+            .setThumbnail("https:"+result[0].avatar)
+            .addFields(
+                {name:'Name',value:result[0].firstName+result[0].lastName},
+                {name:'Rank',value:result[0].rank},
+                {name:'Rating',value:result[0].rating},
+
+            );
+            message.channel.send(embed);
+            
+        }
+    
 });
 
 
